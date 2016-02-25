6f09ac264b939d9b3cc864f002d57c4c3019154f
IMPELLO\rwoolmertyler <IMPELLO\rwoolmertyler@hq-lap-2186.impello.co.uk>
2016-02-25 17:34:59 +0000

    Signed-off-by: IMPELLO\rwoolmertyler <IMPELLO\rwoolmertyler@hq-lap-2186.impello.co.uk>

--- src/main/java/com/rupert/hangman/HangmanLogic.java
+++ src/main/java/com/rupert/hangman/HangmanLogic.java
@@ -4,10 +4,14 @@ package com.rupert.hangman;
  * Hello world!
  * Bye Bye world!
  */
-public class App 
+public class HangmanLogic 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
     }
+    
+    
+    
 }
---
+++ src/test/java/com/rupert/hangman/HangmanLogicTest.java
@@ -0,0 +1,16 @@
+package com.rupert.hangman;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class HangmanLogicTest {
+  
+	HangmanLogic engine = new HangmanLogic();
+	
+	@Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
