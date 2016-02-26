8165bef77d7eca4c41d9bfdd5a677b8b0e288856
IMPELLO\rwoolmertyler <IMPELLO\rwoolmertyler@hq-lap-2186.impello.co.uk>
2016-02-26 13:21:33 +0000

    Signed-off-by: IMPELLO\rwoolmertyler <IMPELLO\rwoolmertyler@hq-lap-2186.impello.co.uk>

--- src/main/java/com/rupert/hangman/HangmanGame.java
+++ src/main/java/com/rupert/hangman/HangmanGame.java
@@ -1,16 +1,17 @@
 package com.rupert.hangman;
 
 import java.awt.EventQueue;
+import java.awt.Image;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 
 public class HangmanGame {
 
 	private JFrame frame;
 
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -24,20 +25,34 @@ public class HangmanGame {
 		});
 	}
 
-	/**
-	 * Create the application.
-	 */
+
 	public HangmanGame() {
 		initialize();
 	}
 
-	/**
-	 * Initialize the contents of the frame.
-	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(0, 100, 450, 300);
+		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 
+	public void init_icons() {
+		
+		try{
+			Image x = ImageIO.read(HangmanGame.class.getResource("/x.png"));
+			Image o = ImageIO.read(HangmanGame.class.getResource("/o.png"));
+			
+//			xIcon = new ImageIcon(x.getScaledInstance(grid[0].getWidth(), grid[0].getHeight(),Image.SCALE_SMOOTH));
+//			oIcon = new ImageIcon(o.getScaledInstance(grid[0].getWidth(), grid[0].getHeight(),Image.SCALE_SMOOTH));
+//			
+//			for(int i=0; i<9; i++) {
+//				if (grid[i].getIcon() != null) 
+//						grid[i].setIcon(board[i] == 1 ? xIcon : oIcon);
+//			}
+			
+		}catch(IOException ex) {
+			ex.printStackTrace();
+		}
+	}
+	
 }
